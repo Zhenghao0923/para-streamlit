f360fe7e258661da7faebb0bec440a45bb02902b
Zhenghao0923 <43866182+Zhenghao0923@users.noreply.github.com>
2023-12-22 17:47:48 +0800

    Add files via upload

---
+++ homepage.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="mult"
+)
+st.title("main page")
+st.sidebar.success("select your page")
+
+#to get input from user
+if "input" not in st.session_state:
+    st.session_state["input"] =""
+
+input = st.text_input("Input your info here",st.session_state["input"])
+submit = st.button("Submit-提交")
+if submit:
+    st.session_state["input"] = input
+    st.write("Your have entered: ",input)
+    
